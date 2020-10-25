f7e34261168d8f7f2816c62b8af7bf57218a813c
Nicolaskcd <nicolasskcc@gmail.com>
2020-10-25 01:03:49 -0300

    GoodPlay: Creando pagina de inicio

--- GoodPlay/GoodPlay/settings.py
+++ GoodPlay/GoodPlay/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'GoodPlay.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- GoodPlay/Login/views.py
+++ GoodPlay/Login/views.py
@@ -1,2 +1,16 @@
 from django.shortcuts import render
+from . models import Juego, Compañia
+from django.views import generic
 
+# Create your views here.
+def index(request):
+    num_Juego = Juego.objects.all().count()
+    num_Juego_available=Juego.objects.filter(juego__exact='E').count()
+    num_compañias = Compañia.objects.all().count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_Juego': num_Juego, 
+        'num_Juego_available': num_Juego_available, 'num_compañias' : num_compañias}, 
+    )
